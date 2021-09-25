e710e48a64cefa8bdef36d9039fa88c8c90e8781
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2021-09-26 02:57:26 +0300

    Update apiService.js

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,12 +1,13 @@
-const base = 'https://pixabay.com/api/';
+const base = `https://pixabay.com/api/`;
+const url = `?image_type=photo&orientation=horizontal&q=`;
 
 export default {
     page: 1,
     query: '',
     async fetcArticles(query) {
         const key = '23515098-6ca4d9d130a27025c1d21d34d';
-        const param = `?image_type=photo&orientation=horizontal&q=${this.query}&page=${this.page}&per_page=12&key=${key}`;
-        const sum = await fetch(base + param);
+        const param = `${base}${url}${this.query}&page=${this.page}&per_page=12&key=${key}`;
+        // const sum = await fetch(base + param);
         const resp = await sum.json();
         this.bigest();
         return resp.u
