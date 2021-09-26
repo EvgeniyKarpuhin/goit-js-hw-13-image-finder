91840b14ea973fe0f5216b9574f0cbbf48b4c51f
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2021-09-26 21:01:32 +0300

    Update apiService.js

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,26 +1,32 @@
-const base = `https://pixabay.com/api/`;
+import Notiflix from 'notiflix';
+const base = 'https://pixabay.com/api/';
 const url = `?image_type=photo&orientation=horizontal&q=`;
+const key = '23515098-6ca4d9d130a27025c1d21d34d';
 
 export default {
     page: 1,
     query: '',
     async fetcArticles(query) {
-        const key = '23515098-6ca4d9d130a27025c1d21d34d';
         const param = `${base}${url}${this.query}&page=${this.page}&per_page=12&key=${key}`;
         // const sum = await fetch(base + param);
         // const resp = await sum.json();
-        return fetch(param);
+        return fetch(param)
         // this.bigest();
         // return resp.u
+        .then(response => {
+            if(response.ok) return response.json();
+        })
+        .then(data => {
+            this.page += 1;
+            return data;
+        })
+        .catch(err => Notiflix.Notify.failure(`${err}`));
     },
     get search() {
         return this.query;
     },
     set search(string) {
         this.query = string;
-    },
-    bigest() {
-        this.page += 1;
     },
     reset() {
         this.page = 1;
