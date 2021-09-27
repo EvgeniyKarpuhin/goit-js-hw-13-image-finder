293154e24ff58512f09f2a87d9a0c8982c4cb17a
EvgeniyKarpuhin <evgeniygalaxy4@gmail.com>
2021-09-27 19:38:23 +0300

    Update searchImg.js

--- src/js/searchImg.js
+++ src/js/searchImg.js
@@ -1,48 +1,97 @@
-import service from './apiService';
-import templ from '../templ/templ.hbs';
+// import service from './apiService';
+// import templ from '../templ/templ.hbs';
+// import refs from './refs';
+
+
+// refs.searchForm.addEventListener('submit', imgSearch);
+// refs.loadMore.addEventListener('click', loadMoreBtn);
+
+// function imgSearch(e) {
+//     e.preventDefault();
+
+//     const form = e.currentTarget;
+//     const input = form.elements.query;
+
+//     clearList();
+
+//     service.reset();
+//     service.search = input.value;
+
+//     service.fetcArticles().then(hits => {
+//         const build = buildList(hits);
+//         listItem(build);
+//     });
+//     input.value = '';
+// }
+
+//     function loadMoreBtn() {
+//     service.fetcArticles().then(hits => {
+//         const build = buildList(hits);
+//         listItem(build);
+//         window.scrollTo(0, 1000);
+
+//         window.scrollTo({
+//             top: 1000,
+//             behavior: 'smooth',
+//         });
+//     });
+// }
+
+// function listItem(items) {
+//     refs.gallery.insertAdjacentHTML('beforeend', items);
+// }
+// function buildList(items) {
+//     return templ(items);
+// }
+// function clearList() {
+//     refs.gallery.innerHTML = '';
+// }
+
+import servise from './apiService';
+import cardImeges from '../templ/templ.hbs';
 import refs from './refs';
 
 
-refs.searchForm.addEventListener('submit', imgSearch);
-refs.loadMore.addEventListener('click', loadMoreBtn);
+refs.searchForm.addEventListener('submit', imageSearchInputHandler);
+refs.loadMoreBtn.addEventListener('click', loadMoreBtnHandler);
 
-function imgSearch(e) {
-    e.preventDefault();
+function imageSearchInputHandler(e) {
 
-    const form = e.currentTarget;
-    const input = form.elements.query;
+  e.preventDefault();
 
-    clearList();
+  const form = e.currentTarget;
+  const input = form.elements.query;
 
-    service.reset();
-    service.search = input.value;
+  clearListItems();
 
-    service.fetcArticles().then(hits => {
-        const build = buildList(hits);
-        listItem(build);
+  servise.resetPage();
+  servise.searchQuerry = input.value;
+
+  servise.fetcArticles().then(hits => {
+    const markup = buildListItemsTemplate(hits);
+    iserListItems(markup);
+  });
+  input.value = '';
+}
+
+function loadMoreBtnHandler() {
+  servise.fetcArticles().then(hits => {
+    const markup = buildListItemsTemplate(hits);
+    iserListItems(markup);
+    window.scrollTo(0, 1000);
+
+    window.scrollTo({
+      top: 1000,
+      behavior: 'smooth',
     });
-    input.value = '';
+  });
 }
-
-    function loadMoreBtn() {
-    service.fetcArticles().then(hits => {
-        const build = buildList(hits);
-        listItem(build);
-        window.scrollTo(0, 1000);
-
-        window.scrollTo({
-            top: 1000,
-            behavior: 'smooth',
-        });
-    });
+function iserListItems(items) {
+  refs.gallery.insertAdjacentHTML('beforeend', items);
 }
-
-function listItem(items) {
-    refs.gallery.insertAdjacentHTML('beforeend', items);
+function buildListItemsTemplate(items) {
+  return cardImeges(items);
 }
-function buildList(items) {
-    return templ(items);
-}
-function clearList() {
-    refs.gallery.innerHTML = '';
+function clearListItems() {
+  refs.gallery.innerHTML = '';
 }
